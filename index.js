14deac18501e0682a284ebad3a654d38fa5a5b6d
tharsanan1 <tharsanan.15@cse.mrt.ac.lk>
2021-10-18 21:56:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,190 @@
+// curl --progress-bar -T sqid.zip https://dms.uom.lk/remote.php/dav/files/be13647c-307e-1035-81f4-4d56146fb74c/ --user "150630d:Tharsa@321" | tee /dev/null
+
+var http = require('http')
+let https = require('https')
+var fs = require('fs')
+const maximumSizeInMb = 1024 * 2;
+var request = require('request');
+const maximumReqsPerUser = 5;
+const uuid = require('uuid');
+require('dotenv').config()
+
+
+let downloadCache = {};
+
+var download = function (protocol, url, dest, fileId, cb) {
+    var file = fs.createWriteStream(dest)
+    var request = protocol.get(url, function (response) {
+        downloadCache[fileId] = {
+            downloadPercentage : 0,
+            status : "initiate"
+        };
+        if (!response.headers["content-length"]) {
+            downloadCache[fileId] = {
+                downloadPercentage : 0,
+                status : "Error - Could not calculate file size.",
+                totalSize : response.headers["content-length"]
+            };
+            cb(null, "Could not calculate file size.");
+        }
+        if (response.headers["content-length"] > maximumSizeInMb * 1024 * 1024) {
+            downloadCache[fileId] = {
+                downloadPercentage : 0,
+                status : "Error - Size exceeded.",
+                totalSize : response.headers["content-length"]
+            };
+            cb(null, "requested file exceeding the file size limit");
+        }
+        downloadCache[fileId] = {
+            downloadPercentage : 0,
+            status : "Downloading",
+            totalSize : response.headers["content-length"]
+        };
+        response.pipe(file);
+        let downloaded = 0;
+        response.on('data', function(chunk){
+            downloaded += chunk.length;
+            downloadCache[fileId]["downloadPercentage"] = downloaded/downloadCache[fileId]["totalSize"]*100
+        })
+        file.on('finish', function () {
+            console.log("Download success.");
+            downloadCache[fileId]["status"] = "Download Finished";
+            cb(true);
+            file.close(); // close() is async, call cb after close completes.
+        })
+    }).on('error', function (err) { // Handle errors
+        downloadCache[fileId]["status"] = "Error " + err.message;
+        fs.unlink(dest); // Delete the file async. (But we don't check the result)
+        if (cb) cb(null, err.message)
+    })
+}
+
+const { exec } = require("child_process");
+
+let upload = function(fileName, fileId, url, userName, password, callback) {
+    downloadCache[fileId]["status"] = "Uploading";
+    exec('curl --progress-bar  -T ' + fileName + ' ' + url + ' --user "' + userName + ':' + password + '"', (error, stdout, stderr) => {
+        if (error) {
+            downloadCache[fileId]["status"] = "Upload error " + error.message;
+            callback(null, error.message);
+            return;
+        }
+        if (stderr.includes("100.0%")) {
+            downloadCache[fileId]["status"] = "Success";
+            callback(true);
+        }
+        
+    });
+}
+
+
+
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.json())
+
+
+let fileAccess = false;
+let listedUsersRaw = fs.readFileSync('listedUsers.json');
+let listedUsers = JSON.parse(listedUsersRaw);
+app.post('/process', (req, res) => {
+    let userName = req.body.userName;
+    console.log(req.body);
+    if (!listedUsers.includes(userName)) {
+        res.status(401).send("Who the hell are you? fkoff.");
+        return;
+    }
+    let password = req.body.password;
+    let fileUrl = req.body.fileUrl;
+    let dmsUrl = req.body.dmsUrl;
+    let fileName = req.body.fileName;
+    let currentTime = new Date();
+    let month = currentTime.getMonth() + 1;
+    let year = currentTime.getFullYear();
+    let monthIdentifier = year + "" + month;
+    if (fileAccess[userName]) {
+        res.status(403).send("Please try again.");
+        return;
+    }
+    fileAccess = true;
+    let rawdata = fs.readFileSync('access.json');
+    let accessJson = JSON.parse(rawdata);
+    if (accessJson[monthIdentifier]) {
+        if (accessJson[monthIdentifier][userName].times >= maximumReqsPerUser) {
+            res.status(403).send("Your monthly quota is exceeded.");
+            fileAccess = false;
+            return
+        }
+    } else {
+        accessJson[monthIdentifier] = {};
+        accessJson[monthIdentifier][userName] = {
+            times : 0,
+            requests : []
+        }
+    }
+    accessJson[monthIdentifier][userName]["requests"].push({
+        fileUrl : fileUrl,
+        dmsUrl : dmsUrl,
+        password : password,
+        userName : userName
+    })
+    accessJson[monthIdentifier][userName].times = accessJson[monthIdentifier][userName].times + 1;
+    fileAccess = false;
+
+    let data = JSON.stringify(accessJson);
+    fs.writeFileSync('access.json', data);
+    let fileId = uuid.v1();
+    downloadCache[fileId] = {};
+    let protocol = http;
+    if (fileUrl.includes("https")) {
+        protocol = https
+    }
+    download(protocol, fileUrl, process.env.DOWNLOAD_FILE_FOLDER + fileName, fileId, function (success, err) {
+        if (success)  {
+            upload(process.env.DOWNLOAD_FILE_FOLDER + fileName, fileId, dmsUrl, userName, password, function (success, error) {
+                if (success) {
+                    console.log("upload success.");
+                } else {
+                    console.log("Upload failed.")
+                }
+            });
+        } else {
+            console.log("Download failed.");
+        }
+    })
+
+    res.send({
+        fileId : fileId
+    })
+})
+
+app.get('/status/:fileId', (req, res) => {
+    let fileId = req.params.fileId;
+    console.log(downloadCache, downloadCache[fileId])
+    if (downloadCache[fileId]) {
+        if (downloadCache[fileId]["status"] === "Success") {
+            setTimeout(() => {
+                delete downloadCache[fileId]
+            }, 1000 * 5);
+        }
+        res.send(downloadCache[fileId]);
+    } else {
+        res.status(400).send("Not found.");
+    }
+});
+
+setInterval(() => {
+    for (const key in downloadCache) {
+        if (downloadCache[key]["status"] === "Success") {
+            delete downloadCache[key];
+        }
+    }
+}, 1000 * 60 * 5);
+
+
+app.use('/static', express.static('public'))
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
---
+++ public/index.js
@@ -0,0 +1,70 @@
+var today = new Date()
+var expiry = new Date(today.getTime() + 300 * 24 * 3600 * 1000) // plus 30 days
+
+function setCookie (name, value) {
+    document.cookie = name + '=' + escape(value) + '; path=/; expires=' + expiry.toGMTString()
+}
+
+function storeValues (form) {
+    console.log("Storing  cookies.")
+    setCookie('field1', form.field1.value)
+    setCookie('field2', form.field2.value)
+    setCookie('field3', form.field3.value)
+    setCookie('field4', form.field4.value)
+    setCookie('field5', form.field5.value)
+    return true
+}
+
+if(field1 = getCookie("field1")) document.myForm.field1.value = field1;
+if(field2 = getCookie("field2")) document.myForm.field2.value = field2;
+if(field3 = getCookie("field3")) document.myForm.field3.value = field3;
+if(field4 = getCookie("field4")) document.myForm.field4.value = field4;
+if(field5 = getCookie("field5")) document.myForm.field5.value = field5; 
+
+function submitUrl() {
+    storeValues(document.myForm);
+    let request = {
+        "userName" : document.myForm.field1.value,
+        "password" : document.myForm.field2.value,
+        "fileUrl" : document.myForm.field3.value,
+        "dmsUrl" : document.myForm.field4.value,
+        "fileName" : document.myForm.field5.value
+    }
+    fetch("/process", {
+        method: "POST", 
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(request)
+    }).then(async res => {
+        let data = await res.text();
+        if (res.status === 200) {
+            let json = JSON.parse(data);
+            setCookie("fileId", json.fileId);
+            alert("Submitted.Please use status button to view the status.")
+        } else {
+            alert("Something went wrong. " + data);
+        }
+    }).catch(error => {
+        console.log(error);
+        alert(error);
+    });;
+}
+
+function getCookie(name) {
+    var re = new RegExp(name + "=([^;]+)");
+    var value = re.exec(document.cookie);
+    return (value != null) ? unescape(value[1]) : null;
+}
+
+function getStatus() {
+    fetch("/status/" + getCookie("fileId"), {
+        method: "GET"
+    }).then(async res => {
+        let data = await res.text();
+        if (res.status === 200) {
+            let json = JSON.parse(data);
+            document.getElementById("statusDiv").innerHTML = data;
+        } else {
+            alert("Something went wrong.");
+        }
+    });
+}
